92cab9acff97b7f29a791bd44dc18d1cbbc7912c
MrSmoKkkk98 <105886325+MrSmoKkkk98@users.noreply.github.com>
2022-12-29 02:48:59 +0200

    exercise_4

--- day_18/turtle_4.py
+++ day_18/turtle_4.py
@@ -1,41 +1,27 @@
-from turtle import Turtle, Screen
+import turtle as t
 import random
 
-turt = Turtle()
+turt = t.Turtle()
+t.colormode(255)
+
+def random_color():
+    r = random.randint(0, 255)
+    g = random.randint(0, 255)
+    b = random.randint(0, 255)
+    color = (r, g, b)
+    return color
+
+directions = [0, 90, 180, 270]
 turt.shape("classic")
-turt.speed(1)
-
-turt.pos()
-turt.distance(30, 40)
-pen = Turtle()
-pen.forward(177)
-turt.distance(pen)
-
-# color = random.choice()
-    
+turt.speed(10)
+turt.pensize(30)
+for _ in range(50):
+    turt.forward(30)
+    turt.setheading(random.choice(directions))
+    turt.color(random_color())
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-screen = Screen()
+screen = t.Screen()
 screen.onclick(screen)
 
