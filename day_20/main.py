e60f396428acf75cc111c758be5b3cdc0e5c98bc
MrSmoKkkk98 <105886325+MrSmoKkkk98@users.noreply.github.com>
2022-12-30 14:38:41 +0200

    Snake game draft

---
+++ day_20/main.py
@@ -0,0 +1,39 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.setup(width=600, height=600)
+screen.bgcolor("black")
+screen.title("The Snake Game")
+
+# 1. Create a snake body
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+screen.exitonclick()
