3519f3f00e069713d54dba87c9320a5ca5b6d7c2
MrSmoKkkk98 <105886325+MrSmoKkkk98@users.noreply.github.com>
2022-12-31 12:51:38 +0200

    Create a snake body

--- day_20_snake_game/main.py
+++ day_20_snake_game/main.py
@@ -6,10 +6,14 @@ screen.bgcolor("black")
 screen.title("The Snake Game")
 
 # 1. Create a snake body
+x_positions = [0, -20, -40]
+all_turtles = []
 
-
-
-
+for i in range(3):
+    turtle = Turtle(shape="square")
+    turtle.color("white")
+    turtle.goto(x=x_positions[i], y=0)
+    all_turtles.append(turtle)
 
 
 
