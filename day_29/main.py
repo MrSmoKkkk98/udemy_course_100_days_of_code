2fe181f490235ef4255d1cfa23f566d1db55ac73
MrSmoKkkk98 <mrsmokkkk@DESKTOP-HDCH670>
2023-01-07 17:13:57 +0200

    Have added a save function which takes user's website name, email and password and creates a file data.txt to add the info there

--- day_29/main.py
+++ day_29/main.py
@@ -1,7 +1,15 @@
 # ---------------------------- PASSWORD GENERATOR ------------------------------- #
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
-
+def save():
+    web_info = website_entry.get()
+    pass_info = password_entry.get()
+    email_info = email_entry.get()
+    
+    with open("day_29/data.txt", "a") as f:
+        f.write(f"{web_info} | {email_info} | {pass_info}\n")
+        website_entry.delete(0, "end")
+        password_entry.delete(0, "end")
 # ---------------------------- UI SETUP ------------------------------- #
 from tkinter import*
 
@@ -28,9 +36,11 @@ password_label.grid(column=0, row=3)
 # Entries
 website_entry = Entry(width=50)
 website_entry.grid(column=1, row=1, columnspan=2)
+website_entry.focus()
 
 email_entry = Entry(width=50)
 email_entry.grid(column=1, row=2, columnspan=2)
+email_entry.insert(0, "smok@gmail.com")
 
 password_entry = Entry(width=32)
 password_entry.grid(column=1, row=3)
@@ -39,7 +49,7 @@ password_entry.grid(column=1, row=3)
 button_generate_password = Button(text="Generate Password")
 button_generate_password.grid(column=2, row=3)
 
-button_add = Button(text="Add", width=48)
+button_add = Button(text="Add", width=48, command=save)
 button_add.grid(column=1, row=4, columnspan=2)
 
 
